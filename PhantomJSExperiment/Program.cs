211a7f3077b1ec7893239314c147844afca65b3a
Joshua Dapitan <joshuadapitan@gmail.com>
2018-06-06 01:37:03 +0800

    Initial commit

---
+++ PhantomJSExperiment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+namespace PhantomJSExperiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PhantomJSDriver driver = new PhantomJSDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate ().GoToUrl ("https://bing.com");
+
+            IWebElement searchField = driver.FindElementById("sb_form_q");
+            searchField.SendKeys ("Jemima Andria");
+            IWebElement btnSearch = driver.FindElementById("sb_form_go");
+            btnSearch.Click ();
+            driver.GetScreenshot().SaveAsFile("screenshot.png", ScreenshotImageFormat.Png);
+            driver.Quit();
+        }
+    }
+}
